5b15092f5b1313b412cd5a2295d05d322600def5
X4PDK0P <xapdkopplay@gmail.com>
2024-03-13 21:49:40 +0300

    Выполненая работа

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
-public interface ListDiffInterface {
-    boolean theSameAs();
-    boolean equals();
+public interface ListDiffInterface<T> {
+    boolean theSameAs(T obj);
+    boolean equals(Object o);
 }
